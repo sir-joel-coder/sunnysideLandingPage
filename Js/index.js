b756cbda03d8f43ba20371a8f00bdd17a8941ae1
sir-joel-coder <elluusiayojoel@gmail.com>
2022-08-11 06:13:23 -0700

    my first commit

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+let navCards = document.getElementById('nav-cards');
+
+
+function displayNavCard(){
+    navCards.style.display = 'flex'
+}
+
+function closeBtn() {
+    navCards.style.display = 'none'
+}
